47f06c25e419781756150389903238b88b4f0af8
Rodrigo Silva <rodrigo.morais@unchained-ia.com>
2025-05-31 12:11:40 +0200

    add user agent only for post

--- hiscores_scrapper.py
+++ hiscores_scrapper.py
@@ -5,7 +5,6 @@ import logging
 import time
 from tenacity import retry, stop_after_attempt, wait_fixed, retry_if_exception_type
 import json
-import random
 
 # force commit 4
 
@@ -15,20 +14,14 @@ FAILED_PLAYERS_FILE = "data/failed_players.json"
 LOGS_FILE = "logs/logs.txt"
 SUCCESS_RESP = (201, 200)
 LAST_PAGE = 16395
-USER_AGENTS = [
-    "Mozilla/5.0 (Linux; Android 8.0; Pixel 2 Build/OPD3.170816.012) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.8592.1188 Mobile Safari/537.36",
-    "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.1456.1529 Mobile Safari/537.36",
-    "Mozilla/5.0 (Linux; Android 8.0; Pixel 2 Build/OPD3.170816.012) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.6915.1989 Mobile Safari/537.36",
-    "Mozilla/5.0 (Linux; Android 8.0; Pixel 2 Build/OPD3.170816.012) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.6453.1890 Mobile Safari/537.36",
-    "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.3342.1121 Mobile Safari/537.36",
-]
+API_HEADERS = {"User-Agent": "leagues-scrapper-bot/1.0"}
 
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
     handlers=[
         logging.FileHandler(LOGS_FILE),
-        logging.StreamHandler(),  # can disable for no console output
+        logging.StreamHandler(),
     ],
 )
 
@@ -38,9 +31,9 @@ logging.basicConfig(
     wait=wait_fixed(7),
     retry=retry_if_exception_type(requests.RequestException),
 )
-def post_player_to_api(name, headers):
+def post_player_to_api(name):
     encoded_name = requests.utils.quote(name)
-    resp = requests.post(API_URL.format(encoded_name))
+    resp = requests.post(API_URL.format(encoded_name), headers=API_HEADERS)
     if resp.status_code not in SUCCESS_RESP:
         logging.error(
             f"Failed to post player {name} to API. Status Code: {resp.status_code}"
@@ -55,11 +48,11 @@ def post_player_to_api(name, headers):
     wait=wait_fixed(7),
     retry=retry_if_exception_type(requests.RequestException),
 )
-def get_page(page, headers) -> html.HtmlElement:
+def get_page(page) -> html.HtmlElement:
     hiscores_url = BASE_URL.format(page)
-    response = requests.get(hiscores_url, headers)
+    response = requests.get(hiscores_url)
 
-    if response.status_code not in (SUCCESS_RESP):
+    if response.status_code not in SUCCESS_RESP:
         logging.error(
             f"Failed to fetch page {page}. Status Code: {response.status_code}"
         )
@@ -91,27 +84,25 @@ def main():
     page = start_from_last_page()
     total_players = 0
     no_players_found = 0
-    try:
 
+    try:
         while page < LAST_PAGE:
-            headers = random.choice(USER_AGENTS)
-            tree = get_page(page, headers)
+            tree = get_page(page)
             names = tree.xpath('//tr[@class="personal-hiscores__row"]/td[2]/a/text()')
+
             if not names:
-                no_players_found += 1
-                if no_players_found >= 3:
-                    logging.info("No players found for 3 consecutive pages. Stopping.")
-                    raise ValueError("No players found for 3 consecutive pages.")
+                raise ValueError("No players found check the page structure")
 
             for name in names:
                 time.sleep(3)
-                post_player_to_api(name, headers)
+                post_player_to_api(name)
                 total_players += 1
                 logging.info(f"Posted player {name} to API.")
 
             page += 1
             failed["last_page"] = page
             save_failed_players(failed)
+
         logging.info(
             f"Finished scrapping. Extracted {total_players} players and Pages: {page}."
         )
@@ -121,9 +112,7 @@ def main():
         )
         failed["last_page"] = page
         save_failed_players(failed)
-        logging.info(f"Failed players: {failed}")
-        logging.info(f"Saved state to {FAILED_PLAYERS_FILE}.")
-        raise e
+        raise
 
 
 if __name__ == "__main__":
