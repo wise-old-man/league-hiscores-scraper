5cda4a240e53b1c4020fdbfff848d6db3cc6c2f9
Rodrigo Silva <rodrigo.morais@unchained-ia.com>
2025-06-22 16:22:35 +0200

    add retry

--- players_scrapper.py
+++ players_scrapper.py
@@ -1,7 +1,9 @@
+import os
 import requests
 import json
 import logging
 from tenacity import retry, stop_after_attempt, wait_fixed, retry_if_exception_type
+import time
 
 OSRS_API_URL = "https://secure.runescape.com/m=hiscore_oldschool_seasonal/ranking.json?table=0&category=1&size=50&toprank={}"
 WOM_API_URL = "https://api.wiseoldman.net/league/players/{}"
@@ -39,23 +41,24 @@ def post_player_to_api(name):
 
 def main():
     try:
+        if not os.path.exists("data/last_rank.txt"):
+            with open("data/last_rank.txt", "w") as f:
+                f.write("0")
         with open("data/last_rank.txt", "r") as f:
             content = f.read().strip()
         curr_rank = int(content) if content else 0
         logging.info(f"Starting from rank {curr_rank}.")
 
         while curr_rank < LAST_RANK:
-            players = (
-                requests.get(OSRS_API_URL.format(curr_rank)).json()
-                if requests.get(OSRS_API_URL.format(curr_rank)).ok
-                else []
-            )
+            resp = requests.get(OSRS_API_URL.format(curr_rank))
+            players = resp.json() if resp.ok else []
 
             if not players:
                 raise ValueError(f"No more players found at rank {curr_rank}.")
 
             for player in players:
                 post_player_to_api(player.get("name"))
+                time.sleep(3)
 
             curr_rank += 50
         logging.info(f"Processed up to rank {curr_rank}.")
