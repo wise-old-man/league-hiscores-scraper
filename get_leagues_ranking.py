62ff53a80ff738bcf3d7d77e7950c47dd57c18e2
Rodrigo Silva <rodrigo.morais@unchained-ia.com>
2025-07-12 22:59:13 +0200

    Save curr rank after page being processed, add retry if fetching players fail

--- get_leagues_ranking.py
+++ get_leagues_ranking.py
@@ -36,6 +36,25 @@ def post_player_to_api(name):
         )
 
 
+def save_curr_rank(curr_rank):
+    with open("data/last_rank.txt", "w") as f:
+        f.write(str(curr_rank))
+
+
+@retry(
+    stop=stop_after_attempt(5),
+    wait=wait_fixed(120),
+)
+def get_players(curr_rank):
+    resp = requests.get(OSRS_API_URL.format(curr_rank))
+    players = resp.json()
+    if not resp.ok or not players:
+        raise ValueError(
+            f"Failed to fetch players from API. Status Code: {resp.status_code}"
+        )
+    return players
+
+
 def main():
     try:
         if not os.path.exists("data/last_rank.txt"):
@@ -43,29 +62,25 @@ def main():
                 f.write("0")
         with open("data/last_rank.txt", "r") as f:
             content = f.read().strip()
+
         curr_rank = int(content) if content else 0
         logging.info(f"Starting from rank {curr_rank}.")
 
         while curr_rank < LAST_RANK:
-            resp = requests.get(OSRS_API_URL.format(curr_rank))
-            players = resp.json() if resp.ok else []
-
-            if not players:
-                raise ValueError(f"No more players found at rank {curr_rank}.")
+            players = get_players(curr_rank)
 
             for player in players:
                 try:
                     post_player_to_api(player.get("name"))
                     time.sleep(1)
                 except:
-                    logging.warn(f"Failed to post the player: {player.get("name")}")
-
+                    logging.warning(f"Failed to post the player: {player.get('name')}")
+            save_curr_rank(curr_rank)
+            logging.info(f"Processed rank page: {curr_rank}.")
             curr_rank += 50
-        logging.info(f"Processed up to rank {curr_rank}.")
     except Exception as e:
         logging.error(f"An error occurred: {e}")
-        with open("data/last_rank.txt", "w") as f:
-            f.write(str(curr_rank))
+        save_curr_rank(curr_rank)
         logging.info(f"Saved last rank {curr_rank} to file.")
         raise e
 
