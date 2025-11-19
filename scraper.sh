f4de74ece5c134e2cbd6beb4ef8c39c3ee60eb7f
Robin <5983417+rorro@users.noreply.github.com>
2025-11-19 01:02:47 +0100

    Let user choose which script to run (#4)

--- scraper.sh
+++ scraper.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
+if [ -z "$1" ]; then
+    echo "Usage: $0 <python_file_name>"
+    exit 1
+fi
+
+PYTHON_FILE="$1"
+
 source .venv/bin/activate
-python3 main.py
+python3 "$PYTHON_FILE"
